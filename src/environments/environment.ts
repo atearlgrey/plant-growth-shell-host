6be25b5384f922f78abdcaed2c45de23062a85fb
hung.nguyenvan <atearlgrey@gmail.com>
2025-10-25 20:54:22 +0700

    chore(env): update realm name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   environment: window?.env?.environment ?? 'development',
   plantGrowthUrl: window?.env?.plantGrowthUrl ?? 'http://localhost:4301',
   authorityUrl: window?.env?.authorityUrl ?? 'http://localhost:8080',
-  authorityRealm: window?.env?.authorityRealm ?? 'master',
+  authorityRealm: window?.env?.authorityRealm ?? 'plant-growth',
   authorityClientId: window?.env?.authorityClientId ?? 'plant-growth-client',
 };
